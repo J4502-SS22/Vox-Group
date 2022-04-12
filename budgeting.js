5b87e55fb6d4720a2ad362439cee7633e92b4682
MaddyRyley <97973085+MaddyRyley@users.noreply.github.com>
2022-04-12 16:13:31 -0500

    created javascript file for budgeting page

---
+++ budgeting.js
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<nav>
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="banking.html">Banking</a></li>
+    <li><a href="investing.html">Investing</a></li>
+    <li><a href="budgeting.html">Budgeting</a></li>
+  </ul>
+</nav>
+
+<h1>Budgeting</h1>
+
+<h2>Expenses Breakdown</h2>
+
+<p>Each month there is a certain amount of money that will be used on different
+  goods and services, like phone bills and food. Some of these expenses cannot be
+  avoided, while others could be considered less necessary and therefore, avoided if need be. </p>
+  <p>When you know how much your after-tax salary is per year, you can find out how much
+    you will have monthly to live off of by dividing the after-tax income by 12, for each of the months.
+    It is suggested by most budgeting tools to divide up your monthly income into three
+    categories: needs, wants, and savings/debt repayment.</p>
+    <p>The 50-30-20 method is widely considered the best way to budget your monthly income.
+      This method accounts for 50% of the monthly income to go to needs, 30% to go to wants,
+      and 20% to go to savings and debt repayments. </p>
+
+    <figure>
+<img src="file:///Users/madisonryley/Desktop/Screen%20Shot%202022-04-11%20at%205.22.53%20PM.png"
+alt="Pie chart broken into three pieces. They are needs, wants and savings and debt repayments.">
+  <figcaption>The 50-30-20 method of monthly budgeting.</figcaption>
+</figure>
+
+<h2>What's a Need?</h2>
+<p>A need is an expense that you absolutely must pay every month just to survive. These costs include:</p>
+<ul>
+<li>Housing</li>
+<li>Groceries</li>
+<li>Utilites</li>
+<li>Transportation</li>
+<li>Insurance</li>
+<li>Loan Repayments</li>
+<li>Pet/Child Care</li>
+</ul>
+
+<h2>What's a Want?</h2>
+<p>A want is an expense that is not necessary to sustain life, but still is something that you'd like to pay for.
+These costs include:</p>
+<ul>
+  <li>New Clothes</li>
+  <li>Sporting Event Ticket</li>
+  <li>Dining Out</li>
+  <li>Concert Tickets</li>
+  <li>Leisure Traveling</li>
+  </ul>
+
+  <h2>What Does Savings and Debt Repayments</h2>
+  <p>Your savings is your sum of money that you have for future expenses, like a wedding, house and retirement. These are funds
+  that you should try to only use when you need to, prior to being used for whatever goal you initially planned.</p>
+  <p>Debt repayment is a bit different than the loan repayments mentioned in the Expenses section of this page.
+  Each month, if you have loans to pay off, there will be a minimum amount that you have to pay.
+If you choose to use some of the 20% category on paying more than the minimum requirement,
+then it falls under debt repayment, according to NerdWallet.</p>
+
+<h2>Cost of Living</h2>
+<p>Depending on where you choose to live
+  and work, your living costs will change. Some cities are more expensive than others, and there are many tools, like
+  NerdWallet’s Cost of living calculator, that can calculate how much it would cost for you to live in a specific place.</p>
+
+  <h2>Cost of Living by Location</h2>
+  <p>Here are four popular cities for recent college graduates and their cost of living per month.
+    Each of these figures take into consideration the average monthly costs from a range of sources and services.</p>
+
+
+
+
+  </body>
+</html>
